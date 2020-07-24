3e2ba69af6edc6650b612b1ff5ec895163ffbf11
Gaboss <zeronet.gp@gmail.com>
2020-07-24 11:57:28 -0400

    nuevas dependencias, Info.js listo y Footer.js en proceso.

--- src/Components/banner.js
+++ src/Components/banner.js
@@ -1,27 +1,46 @@
 import React from 'react';
-import { Jumbotron } from 'react-bootstrap';
+import { Jumbotron, Button, Container, Row, Col } from 'react-bootstrap';
 
-export const Banner = () => {
+export const Banner = (props) => {
+
     return(
         <Jumbotron fluid className='mb-0 p-0'>
-            <div className='banner-img p-0'>
+            <div className='p-0'>
+                <img src={props.img} alt='' className='banner-div' />
             </div>
-            <div className='caption-img animate__animated animate__slideInLeft animate__slow'>
-                <h3 className='pl-3'>Tu mejor opcion en</h3>
-                <h1 className='pl-3'>Soluciones</h1>
-                <h1 className='bg-kpi-text'>Tecnologicas</h1>
+            <div className='caption-img text-center'>
+                <h1>{props.titleH1}</h1>
+                <h2 className='bg-kpi-text'>{props.titleH2}</h2>
             </div>
         </Jumbotron>
     )
 }
 
-export const MBanner = () => {
-    return(
-        <div>
-            <div className='mid-banner'>
-                <h1>BIENVENIDOS</h1>
-                <p></p>
+export const MBanner = (props) => {
+    const row = props.row;
+
+    if (row) {
+        return(
+            <Container fluid className='px-0 mid-banner text-white p-5'>
+                <Row >
+                    <Col xs={2} className='text-center'>
+                        <h1>{props.tittle}</h1>
+                    </Col>
+                    <Col>
+                        <p className='p-4'>{props.text}</p>
+                        <Button href='#' className='hvr-rectangle-in contact-btn float-right' size='lg'>CONTACTANOS</Button>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
+        return(
+        <Container fluid className='px-0'>
+            <div className='mid-banner text-white p-5 text-center'>
+                <h1>{props.tittle}</h1>
+                <p className='p-4'>{props.text}</p>
+                <Button href='#' className='hvr-rectangle-in contact-btn' size='lg'>CONTACTANOS</Button>
             </div>
-        </div>
+        </Container>
     )
 }
--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -4,17 +4,17 @@ import Logo from "../img/Logo KPI Photoshop 1.png"
 
 function NavKPI(){
     return(
-        <Navbar bg='white' expand='lg' className='sticky-top menu-bar'>
+        <Navbar expand='lg' className=' menu-bar position-absolute'>
             <Navbar.Brand href="#home" className='mx-2 menu-icon'>
-                <img alt="" src={Logo} width="90" height="70" className="d-inline-block" />
+               <img alt="" src={Logo} width="90" height="70" className="d-inline-block" />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls='basic-navbar-nav' />
             <Navbar.Collapse id='basic-navbar-nav'>
                 <Nav className="m-auto menu-item text-center align-middle">
-                    <Nav.Link href="#home" className='m-icon hvr-underline-from-center'>Inicio</Nav.Link>
-                    <Nav.Link href="#link" className='m-icon hvr-underline-from-center'>KPI</Nav.Link>
-                    <Nav.Link href="#link" className='m-icon hvr-underline-from-center'>Servicios</Nav.Link>
-                    <Nav.Link href="#link" className='m-icon hvr-underline-from-center'>Contactanos</Nav.Link>
+                    <Nav.Link href="#home" className='m-icon hvr-reveal'>Inicio</Nav.Link>
+                    <Nav.Link href="../pages/kpi.js" className='m-icon hvr-reveal'>KPI</Nav.Link>
+                    <Nav.Link href="#link" className='m-icon hvr-reveal'>Servicios</Nav.Link>
+                    <Nav.Link href="#link" className='m-icon hvr-reveal'>Contactanos</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
