e4502292e3731ad938c12974501c6ff706179dfa
Rafael <Bytheway0708@gmail.com>
2020-08-02 15:23:06 -0400

    Correcciones #1

--- src/Components/info.js
+++ src/Components/info.js
@@ -2,37 +2,37 @@ import React from 'react';
 import {Container, Row, Col, Button} from 'react-bootstrap'
 
 // Titulo Principal
-export const InfoTitle = (props) => {
+export const InfoTitle = ({titleInfo}) => {
     return(
         <Container>
             <Row className='mx-auto my-4 title-kpi w-25'>
-                <h1 className='p-2'>{props.titleInfo}</h1>
+                <h1 className='p-2'>{titleInfo}</h1>
             </Row>
         </Container>
     )
 }
 
 // Titulo Secundario
-export const SecondTitle = props => {
-    return <h2 className={`${props.Titleright ? "text-right" : "text-left" }`}>{props.STitle}</h2>
+export const SecondTitle = ({Titleright,STitle}) => {
+    return <h2 className={`${Titleright ? "text-right" : "text-left" }`}>{STitle}</h2>
 }
 
 // Imagenes Redondas
-export const ImagenCircle = (props) => {
+export const ImagenCircle = ({img}) => {
     return(
             <Col xs={4}>
-                <img src={props.img} alt='' className='mx-auto my-auto d-block img-serv img-fluid'/>
+                <img src={img} alt='' className='mx-auto my-auto d-block img-serv img-fluid'/>
             </Col>
     )
 }
 
 // Texto de Informtacion
-export const TextCircle = (props) => {
+export const TextCircle = ({titleInfo,Titleright,BtnInfo,textInfo,left}) => {
     return(
             <Col>
-                <SecondTitle STitle={props.titleInfo} Titleright={props.Titleright} className='my-3 text-right'/>
-                <p className='text-justify'>{props.textInfo}</p>
-                <Button href='#' className={`contact-btn btn-kpi ${props.left ? "float-left" : "float-right"}`} size='lg'>{props.BtnInfo}</Button>
+                <SecondTitle STitle={titleInfo} Titleright={Titleright} className='my-3 text-right'/>
+                <p className='text-justify'>{textInfo}</p>
+                <Button href='#' className={`contact-btn btn-kpi ${left ? "float-left" : "float-right"}`} size='lg'>{BtnInfo}</Button>
             </Col>
     )
 }
